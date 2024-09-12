c577de272761c85f7674b43550a8a65ba3722d9e
tauzn-clock <zhongdaoxin1999@gmail.com>
2024-09-12 04:09:47 +0000

    working point cloud

--- scripts/depth_anything.py
+++ scripts/depth_anything.py
@@ -11,7 +11,7 @@ import data_conversion
 import message_filters
 import depth_anything_interface
 from std_msgs.msg import Header
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import Image, CameraInfo
 from cv_bridge import CvBridge
 
 class DepthAnything(object):
@@ -32,11 +32,13 @@ class DepthAnything(object):
         # Params
         self.image = None
         self.depth = None
+        self.camera_intrinsic = None
         self.br = CvBridge()
         self.header = Header()
         self.header.frame_id = "depth_anything_optical_frame"
 
         # Publishers
+        self.camera_pub = rospy.Publisher('depth_anything_camera_info', CameraInfo, queue_size=1)
         self.img_pub = rospy.Publisher('depth_anything_img', Image, queue_size=1)
         self.depth_float32_pub = rospy.Publisher('depth_anything_est_depth_float32', Image, queue_size=1)
 
@@ -51,19 +53,26 @@ class DepthAnything(object):
         est_depth = self.model.infer_image(self.image)
 
         self.depth, _ = depth_anything_interface.get_pred_depth(gt_depth, est_depth, self.config, depth_anything_interface.estimated_depth_model, verbose=False)
+      
         msg = self.br.cv2_to_imgmsg(self.depth, "32FC1")
         msg.header = self.header
         self.depth_float32_pub.publish(msg)
+        img_sub_msg.header = self.header
         self.img_pub.publish(img_sub_msg)
+        self.camera_intrinsic.header = self.header
+        self.camera_pub.publish(self.camera_intrinsic)
 
         rospy.loginfo("Time Taken: {}".format((rospy.get_rostime()-sta).to_sec()))
 
     def start(self):
+        # Grab camera intrinsics
+        self.camera_intrinsic = rospy.wait_for_message('/camera/color/camera_info', CameraInfo, timeout=5)
+
         # Subscribers
         img_sub = message_filters.Subscriber(self.camera_topic , Image)
         depth_sub = message_filters.Subscriber(self.depth_topic , Image)
 
-        ts = message_filters.ApproximateTimeSynchronizer([img_sub, depth_sub], queue_size=10, slop=0.05)
+        ts = message_filters.ApproximateTimeSynchronizer([img_sub, depth_sub], queue_size=1, slop=0.05)
         ts.registerCallback(self.callback)       
 
         rospy.spin()
