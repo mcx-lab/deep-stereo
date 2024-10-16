01b66f2e6341921ceab14b96b8cfa653127cdd84
tauzn-clock <zhongdaoxin1999@gmail.com>
2024-10-16 05:06:40 +0000

    update pcd_utils with new scaling

--- scripts/pcd_utils.py
+++ scripts/pcd_utils.py
@@ -48,70 +48,87 @@ def pcd_transform(ori, new, epsilon, trials, verbose = False):
 
     return best_rotation
 
-def pcd_matching_tf(ori, new, epsilon, trials, verbose = False):
-    mean_ori = np.mean(ori, axis=0)
-    translated_ori = ori - mean_ori
-    scale_ori = (np.linalg.norm(translated_ori) ** 2 / len(translated_ori)) ** 0.5
-    rescaled_ori = translated_ori / scale_ori
-
-    mean_new = np.mean(new, axis=0)
-    translated_new = new - mean_new
-    scale_new = (np.linalg.norm(translated_new) ** 2 / len(translated_new)) ** 0.5
-    rescaled_new = translated_new / scale_new
-
-    # Find the best rotation matrix
-    best_rotation = np.eye(3)
+def pcd_matching_tf(coord1, coord2, epsilon, trials, ransac_sample=3, verbose = False):
     best_inliers = 0
+    best_tf = np.eye(4)
 
     for _ in range(trials):
-        i = np.random.randint(len(rescaled_ori))
+        sample_indices = np.random.choice(len(coord1), ransac_sample, replace=False)
+        sample1 = coord1[sample_indices]
+        sample2 = coord2[sample_indices]
 
-        rotation = rotation_matrix_from_vectors(rescaled_new[i], rescaled_ori[i])
+        # Zero Mean
+        sample1_mean = np.mean(sample1, axis=0)
+        sample2_mean = np.mean(sample2, axis=0)
 
-        dist = rescaled_ori - rescaled_new @ rotation.T
+        sample1 = sample1 - sample1_mean
+        sample2 = sample2 - sample2_mean
 
-        inliers = np.mean(np.linalg.norm(dist, axis=1) < epsilon)
+        # SVD
+        H = sample2.T @ sample1
+        try:
+            U, D, V_T = np.linalg.svd(H)
+        except:
+            continue
+        S = np.eye(3)
+        det = np.linalg.det(U) * np.linalg.det(V_T.T)
+        
+        # Check for reflection case
+        if not np.isclose(det,1.):
+            S[3-1,3-1] = -1
 
-        if inliers > best_inliers:
+        R = U @ S @ V_T
+        R = U @ V_T
+
+        sample2 = sample2 @ R.T
+
+        scale = np.einsum('ij,ij->', sample1, sample2) / np.einsum('ij,ij->', sample2, sample2)
+        #print(np.einsum('ij,ij->', sample1, sample2), np.einsum('ij,ij->', sample2, sample2))
+
+        if scale < 1e-5:
+            continue
+            
+        test_coord2 = (coord2 - sample2_mean) * scale
+        test_coord2 = test_coord2 @ R.T + sample1_mean
+
+        error = np.linalg.norm(test_coord2 - coord1, axis=1)
+
+        inliers = np.mean(error < epsilon)
+
+        if (inliers > best_inliers):
             best_inliers = inliers
-            best_rotation = rotation
+
+            tf = np.eye(4)
+            tf[:3,3] = -sample2_mean.T
+
+            new_tf = np.eye(4)
+            new_tf[3,3] = 1/scale
+            tf = new_tf @ tf
+
+            new_tf = np.eye(4)
+            new_tf[:3,:3] = R
+            tf = new_tf @ tf
+
+            new_tf = np.eye(4)
+            new_tf[:3,3] = sample1_mean.T
+            tf = new_tf @ tf
+
+            best_tf = tf
 
     if verbose:
         print(f'Inliers Ratio: {best_inliers}')
 
-    tf = np.eye(4)
-
-    # Translate by -mean_new
-    tf[:3,3] = -mean_new.T
-
-    # Rescale by scale_new
-    new_tf = np.eye(4)
-    new_tf[3,3] = scale_new
-    tf = new_tf @ tf
-    
-    # Rotate by best_rotation
-    new_tf = np.eye(4)
-    new_tf[:3,:3] = best_rotation
-    tf = new_tf @ tf
-
-    # Scale by inverse of scale_ori
-    new_tf = np.eye(4)
-    new_tf[3,3] = 1/scale_ori
-    tf = new_tf @ tf
-
-    # Translate by +mean_ori
-    new_tf = np.eye(4)
-    new_tf[:3,3] = mean_ori.T
-    tf = new_tf @ tf
-
-    return tf
+    return best_tf
 
 def ply_from_1x4_coord(coord, filename, color = None, voxel_size = 0.05):
     pcd = o3d.geometry.PointCloud()
     pcd.points = o3d.utility.Vector3dVector(coord[:,:3]/coord[:,3][:,None])
     if color is not None:
         pcd.colors = o3d.utility.Vector3dVector(color)
-    voxel_grid = pcd.voxel_down_sample(voxel_size=0.05)
+
+    pcd, ind = pcd.remove_statistical_outlier(nb_neighbors=50,
+                                                        std_ratio=1.0)
+    pcd = pcd.voxel_down_sample(voxel_size=0.05)
 
     #o3d.io.write_point_cloud(filename, pcd)
-    o3d.io.write_point_cloud(filename, voxel_grid)
+    o3d.io.write_point_cloud(filename, pcd)
